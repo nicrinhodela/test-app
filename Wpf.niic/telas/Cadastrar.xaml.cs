3d8d33cd0cb07afa624f58696d5109aa2b34a53e
nicrinhodela <126517442+nicrinhodela@users.noreply.github.com>
2023-03-28 17:05:32 -0400

    adicionando arquivo WPF

---
+++ Wpf.niic/telas/Cadastrar.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Wpf.niic.telas
+{
+    /// <summary>
+    /// Lógica interna para Cadastrar.xaml
+    /// </summary>
+    public partial class Cadastrar : Window
+    {
+        private MySqlConnection conexao;
+
+        private MySqlCommand comando;
+
+        public Cadastrar()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Wpf.niic/telas/Listar.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Wpf.niic.telas
+{
+    /// <summary>
+    /// Lógica interna para Listar.xaml
+    /// </summary>
+    public partial class Listar : Window
+    {
+        public Listar()
+        {
+            InitializeComponent();
+        }
+
+        private void DataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+    }
+}
